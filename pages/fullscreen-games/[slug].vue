7987e17bd1fb3c4f302d1bb578156c03b5ee37f6
duclt3005 <duclt3005@gmail.com>
2023-09-30 17:20:49 +0700

    fullgame screen

---
+++ pages/fullscreen-games/[slug].vue
@@ -0,0 +1,79 @@
+<script setup>
+import { useUrlConfig } from "@/composables/useUrlConfig";
+
+const { slug } = useRoute().params;
+const { API_URL, BACKEND_URL } = useUrlConfig();
+
+const { data: game } = await useFetch(() => `${API_URL}/games/${slug}`);
+
+const changeSize = () => {
+  try {
+    const iframe = document.querySelector("#iframe");
+    const iframeCanvas = iframe.contentWindow.document.querySelector("#unity-canvas");
+    if (iframeCanvas) {
+      console.log(iframeCanvas);
+      var style = document.createElement("style");
+      style.textContent = `
+      body {
+        width: 100%;
+        height: 100%;
+      }
+      canvas {
+        width: 100% !important;
+        height: 100% !important;
+      }
+      `;
+      iframe.contentDocument.head.appendChild(style);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+useHead({
+  title: `Publish ${slug} on your website - XGame Studio`,
+  meta: [
+    {
+      name: "description",
+      content:
+        "Games Catalog of XGame Studio. Browse through a collection of high quality, cross platform, HTML5 games and publish them on your website.",
+    },
+    { name: "ogTitle", content: "Games Catalog - XGame Studio" },
+    {
+      name: "ogDescription",
+      content:
+        "Games Catalog of XGame Studio. Browse through a collection of high quality, cross platform, HTML5 games and publish them on your website.",
+    },
+  ],
+});
+</script>
+
+<template>
+  <iframe
+    class="iframe-close"
+    name="iframe"
+    id="iframe"
+    :src="`${BACKEND_URL}/${game.source_link}`"
+    scrolling="no"
+    allowfullscreen
+    frameborder="0"
+    seamless="seamless"
+  ></iframe>
+</template>
+
+<style lang="scss" scoped>
+html,
+body {
+  padding: 0;
+}
+
+iframe {
+  width: 100vw;
+  height: 100vh;
+}
+
+:deep(canvas) {
+  width: 100%;
+  height: 100%;
+}
+</style>

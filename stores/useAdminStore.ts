026513da1f7f5eb62b692f7b685bb9104b07937d
duclt3005 <duclt3005@gmail.com>
2023-10-06 11:59:05 +0700

    admin store

---
+++ stores/useAdminStore.ts
@@ -0,0 +1,90 @@
+import { useHttp } from "@/composables/useHttp";
+import { RESPONSE_STATUS } from "@/constants";
+import { IAuthResponse, ILogin, IRegister, IUser } from "@/types/auth";
+import { IResponseReturn } from "@/types/response";
+import { handleResponse } from "@/utils/functions";
+import { defineStore } from "pinia";
+
+export const useUserStore = defineStore("admin", () => {
+  const user = ref<IUser | null>(null);
+  const token = useCookie("access_token");
+  const isLoggedIn = computed(() => !!user.value);
+
+  async function logout() {
+    const { error } = await useHttp("admin/auth/logout");
+
+    if (error.value) {
+      return {
+        status: RESPONSE_STATUS.ERROR,
+        message: error.value.message,
+      };
+    }
+
+    user.value = null;
+
+    return {
+      status: RESPONSE_STATUS.SUCCESS,
+      message: "Logout successfully!!!",
+    };
+  }
+
+  async function getProfile() {
+    const { data } = await useHttp("admin/auth/profile");
+
+    user.value = data.value as IUser;
+  }
+
+  async function login(loginInfo: ILogin): Promise<IResponseReturn> {
+    const { data, error } = await useHttp<IAuthResponse>("admin/auth/login", {
+      method: "POST",
+      body: loginInfo,
+    });
+
+    if (data.value) {
+      user.value = data.value.user;
+      token.value = data.value.access_token;
+    }
+
+    return handleResponse(data, error, "Login successfully!!!", "Login failed!!!");
+  }
+
+  async function register(registerInfo: IRegister): Promise<IResponseReturn> {
+    const { data, error } = await useHttp<IAuthResponse>("admin/auth/register", {
+      method: "POST",
+      body: registerInfo,
+    });
+
+    if (data.value) {
+      user.value = data.value.user;
+      token.value = data.value.access_token;
+    }
+
+    return handleResponse(data, error, "Register successfully!!!", "Register failed!!!");
+  }
+
+  async function refresh() {
+    const { data, error } = await useHttp<{ access_token: string }>("admin/auth/refresh");
+
+    if (data.value) {
+      localStorage.setItem("access_token", data.value.access_token);
+      return {
+        status: RESPONSE_STATUS.SUCCESS,
+        message: "Refresh token success",
+      };
+    }
+
+    user.value = null;
+    localStorage.removeItem("access_token");
+
+    return {
+      status: RESPONSE_STATUS.FAILED,
+      message: error.value?.message,
+    };
+  }
+
+  function setUser(userData: IUser | null) {
+    user.value = userData;
+  }
+
+  return { user, login, isLoggedIn, getProfile, logout, register, refresh, setUser };
+});
